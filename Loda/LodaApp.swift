1ebc00293917ac8396b57a7688bd1e44a0bb44d3
ConnorColyer <ConnorColyer@icloud.com>
2025-12-23 19:18:38 +0000

    Initial Commit

---
+++ Loda/LodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LodaApp.swift
+//  Loda
+//
+//  Created by Connor Colyer on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LodaTests/LodaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LodaTests.swift
+//  LodaTests
+//
+//  Created by Connor Colyer on 23/12/2025.
+//
+
+import Testing
+@testable import Loda
+
+struct LodaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
